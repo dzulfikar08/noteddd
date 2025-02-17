9ffed2e98a3077c6ddcae26f2892b5034a40bf86
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-02-17 22:53:47 +0800

    feat: add manifest for pwa

---
+++ app/manifest.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Noteddd 🙏🏻',
+    short_name: 'Noteddd 🙏🏻',
+    description: 'Actually note what your boss said to you',
+    start_url: '/',
+    display: 'fullscreen',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  }
+}
